b278517c731545e913a21a6c98b4dec326ba4a30
Sealman <castle2668@gmail.com>
2021-12-14 00:21:00 +0800

    外部依賴改為內部函式參數 > useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -17,22 +17,18 @@ function App() {
     setTasks(loadedTasks);
   };
 
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: 'https://react-http-14f5a-default-rtdb.firebaseio.com/tasks.json',
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   // 將 fetchTasks 加入 dependencies array，以偵測 sendRequest (fetchTasks) 的改變 (如果某些情況下改變了的話)
   // 照理來說是要加入沒錯，但是這樣會造成 Infinite loop...
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks(
+      {
+        url: 'https://react-http-14f5a-default-rtdb.firebaseio.com/tasks.json',
+      },
+      transformTasks
+    );
+  }, [fetchTasks]); // 若加入 fetchTasks 函式，由於函式就是物件，因此每次新生成物件傳的參考都不同，就會一直重跑 useEffect
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
